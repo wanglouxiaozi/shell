4f9a030540a338365e3f53832b9799e1d6d087e5
allenwang <allenwang@tvunetworks.com>
2018-03-05 13:23:41 +0800

    EXPORT 解析
    Change-Id: I66616e4216d7f50cc95b7711699e1641aa274f11

---
+++ EXPORT/demo1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export filename="Tvunetworks"
+
+. demo2 #在子shell环境中运行
+#source demo2 #在当前shell环境中运行
---
+++ EXPORT/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+filename="tvunetworks"
+
+func2()
+{
+	echo "func2"
+	echo $filename
+}
+
+func2
